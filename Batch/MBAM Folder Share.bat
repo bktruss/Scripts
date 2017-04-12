ac59c8b067ea9f15ca38d1e13d214ccf02306e1d
bktruss <bktruss@users.noreply.github.com>
2017-04-12 13:25:21 -0500

    Add files via upload

---
+++ Batch/MBAM Folder Share.bat
@@ -0,0 +1,20 @@
+
+
+
+net share MBAMScanResults="C:\ProgramData\Malwarebytes\MBAMService\ScanResults" /GRANT:Everyone,FULL
+
+net share MBAMQuarantine="C:\ProgramData\Malwarebytes\MBAMService\Quarantine" /GRANT:Everyone,FULL
+
+net share MBAMRtpDetections="C:\ProgramData\Malwarebytes\MBAMService\RtpDetections" /GRANT:Everyone,FULL
+
+net share MBAMArwDetections="C:\ProgramData\Malwarebytes\MBAMService\ArwDetections" /GRANT:Everyone,FULL
+
+net share MBAMAeDetections="C:\ProgramData\Malwarebytes\MBAMService\AeDetections" /GRANT:Everyone,FULL
+
+
+
+
+
+
+
+
